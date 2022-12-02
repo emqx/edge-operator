81e60265352164ec84e34340d7969c4428cd6672
Rory Z <rory-z@outlook.com>
2022-12-02 18:07:28 +0800

    feat: add neuronEX api

--- controllers/neuronex_controller.go
+++ controllers/neuronex_controller.go
@@ -19,12 +19,20 @@ package controllers
 import (
 	"context"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
-	edgeemqxiov1alpha1 "github.com/emqx/edge-operator/api/v1alpha1"
+	edgev1alpha1 "github.com/emqx/edge-operator/api/v1alpha1"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 )
 
 // NeuronEXReconciler reconciles a NeuronEX object
@@ -49,7 +57,54 @@ type NeuronEXReconciler struct {
 func (r *NeuronEXReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	instance := &edgev1alpha1.NeuronEX{}
+	if err := r.Client.Get(ctx, req.NamespacedName, instance); err != nil {
+		if k8sErrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	if instance.GetDeletionTimestamp() != nil {
+		return ctrl.Result{}, nil
+	}
+
+	podList := &corev1.PodList{}
+	if err := r.Client.List(
+		ctx,
+		podList,
+		client.InNamespace(req.Namespace),
+		client.MatchingLabels(instance.GetLabels()),
+	); err != nil {
+		return ctrl.Result{}, err
+	}
+	if len(podList.Items) == 0 {
+		pod := addPod(instance)
+		if err := ctrl.SetControllerReference(instance, pod, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+		if err := r.Client.Create(ctx, pod); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	for _, pod := range podList.Items {
+		controllerRef := metav1.GetControllerOf(&pod)
+		if controllerRef == nil {
+			continue
+		}
+		if controllerRef.UID == instance.UID {
+			if pod.GetDeletionTimestamp() != nil {
+				pod := addPod(instance)
+				if err := ctrl.SetControllerReference(instance, pod, r.Scheme); err != nil {
+					return ctrl.Result{}, err
+				}
+				if err := r.Client.Create(ctx, pod); err != nil {
+					return ctrl.Result{}, err
+				}
+			}
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -57,6 +112,36 @@ func (r *NeuronEXReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 // SetupWithManager sets up the controller with the Manager.
 func (r *NeuronEXReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&edgeemqxiov1alpha1.NeuronEX{}).
+		For(&edgev1alpha1.NeuronEX{}).
+		Watches(
+			&source.Kind{Type: &corev1.Pod{}},
+			&handler.EnqueueRequestForOwner{OwnerType: &edgev1alpha1.NeuronEX{}, IsController: true},
+			builder.WithPredicates(
+				predicate.Funcs{
+					CreateFunc:  func(e event.CreateEvent) bool { return false },
+					UpdateFunc:  func(e event.UpdateEvent) bool { return e.ObjectNew.GetDeletionTimestamp() != nil },
+					DeleteFunc:  func(e event.DeleteEvent) bool { return false },
+					GenericFunc: func(e event.GenericEvent) bool { return false },
+				},
+			),
+		).
 		Complete(r)
+}
+
+func addPod(instance *edgev1alpha1.NeuronEX) *corev1.Pod {
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: instance.Name + "-",
+			Namespace:    instance.Namespace,
+			Labels:       instance.GetLabels(),
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				instance.Spec.Neuron,
+				instance.Spec.EKuiper,
+			},
+		},
+	}
+
+	return pod
 }
